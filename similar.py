fc042c79150700cb4b0eb0722dfa5594e35ce6be
Suit-Of-Sables <suit.of.sables@gmail.com>
2016-12-24 00:06:59 -0500

    made similar.py more, uh, pythonic

--- similar.py
+++ similar.py
@@ -1,16 +1,17 @@
 def find(artist_data, lastfm, config):
+    # non-string config vars seem to be annoying...
+    max_to_add = int(config['DEFAULT']['max_to_add'])
+    min_score = float(config['DEFAULT']['min_score'])
+
     # get list of current similar artists on PTH
     cur_similar = [a['name'] for a in artist_data["similarArtists"]]
 
     # get list of at most max_to_add similar artist from last.fm
-    similar = lastfm.get_artist(artist_data['name']).get_similar()
-    similar = similar[:int(config['DEFAULT']['max_to_add'])]
+    new_similar = lastfm.get_artist(artist_data['name']).get_similar()
+    new_similar = new_similar[:max_to_add]
 
     # find all artists that meet min_score
-    new_similar = []
-    for artist in similar:
-        if artist[1] >= float(config['DEFAULT']['min_score']):
-            new_similar.append(artist[0].name)
+    new_similar = [a[0].name for a in new_similar if a[1] >= min_score]
 
     # but don't don't try to add one if already there
     new_similar = [a for a in new_similar if a not in cur_similar]
